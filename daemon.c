75545ee1022332736ec6448cffa2f02fcaf41752
Bradley <landherrb@gmail.com>
2018-02-23 12:19:24 -0500

    Adding recv loop

--- daemon.c
+++ daemon.c
@@ -15,3 +15,32 @@ typedef struck packet {
    struct icmphdr hdr;
    char msg[MAX_PACKET_SIZE - sizeof(struct icmphdr)];
 } packet;
+
+int main(int argc, char ** argv) {
+   int sock,ret,i,recv_len;
+   packet pkt;
+   
+   struct sock_fprog bpf = {
+      .len = ARRAY_SIZE(code),
+      .filter = code,
+   }
+   
+   sock = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+   
+   if (sock < 0)
+      printf("[-] Could not initialize raw socket\n");
+   
+   ret = setsockopt(sock, SOL_SOCKET, SO_ATTACH_FILTER, &bpf, sizeof(bpf));
+   
+   if(ret < 0)
+      printf("[-] Could not attach bpf filter to socket\n");
+   
+   while(1) {
+      
+      if((recv_len = recv(sock, &pkt, sizeof(pkt), 0) {
+         printf("[+] Got packet!   len:%d    msg:", recv_len);
+      }
+      
+   }
+   close(sock);
+}
