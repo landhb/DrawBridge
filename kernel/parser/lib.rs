9012f118b01816c143c293a44f89803dd063b972
landhb <landherrb@gmail.com>
2020-03-10 22:33:25 -0400

    formatting

--- kernel/parser/lib.rs
+++ kernel/parser/lib.rs
@@ -48,19 +48,19 @@ pub struct Ethernet2Header {
 #[no_mangle]
 pub extern "C" fn validate_packet(c_array: *mut u8, length: usize) -> i32 {
 
-	// we know the memory is pre-allocated in xt_listen
-	// so we can do this without a problem
-	let packet = unsafe { slice::from_raw_parts_mut(c_array, length) };
+    // we know the memory is pre-allocated in xt_listen
+    // so we can do this without a problem
+    let packet = unsafe { slice::from_raw_parts_mut(c_array, length) };
 
-	// validate that this is an IPv4 or IPv6 packet
-	match EtherType::from_u16(BigEndian::read_u16(&packet[12..14])) {
-		Some(EtherType::Ipv4) => EtherType::Ipv4,
-		Some(EtherType::Ipv6) => EtherType::Ipv6,
-		_ => {return -1;}
-	}; 
+    // validate that this is an IPv4 or IPv6 packet
+    match EtherType::from_u16(BigEndian::read_u16(&packet[12..14])) {
+        Some(EtherType::Ipv4) => EtherType::Ipv4,
+        Some(EtherType::Ipv6) => EtherType::Ipv6,
+        _ => {return -1;}
+    }; 
 
 
-	let _eth = Ethernet2Header {
+    let _eth = Ethernet2Header {
         source: {
             let mut result: [u8;6] = [0;6];
             result.copy_from_slice(&packet[6..12]);
@@ -73,7 +73,7 @@ pub extern "C" fn validate_packet(c_array: *mut u8, length: usize) -> i32 {
         },
         ether_type: BigEndian::read_u16(&packet[12..14])
     }; 
-	
-	return 1;
+
+    return 1;
 }
 
