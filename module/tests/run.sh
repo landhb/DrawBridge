40bdafd2f1e9ec6ac54fa10e61916a1956b3843a
landhb <landhb@users.noreply.github.com>
2022-05-19 21:15:56 -0400

    Ensure the ci runner exits with 0

--- module/tests/run.sh
+++ module/tests/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # Optional overrides
 : ${NIXMODULE:=`which nixmodule`}
@@ -22,8 +21,42 @@ else
     $NIXMODULE --debug $NIXMODULEARGS
 fi
 
+# For Github actions
+code=$?
+build=2
+insmod=2
+test=2
+echo "Exited with $code";
+
+# If success all =1
+if [ $code == 0 ]; then build=1 && insmod=1 && test=1; fi;
+
+# If BuildError, then build failed and rest are N/A
+if [ $code == 4 ]; then build=0 && insmod=2 && test=2; fi;
+
+# If InsmodError, then success, fail, N/A
+if [ $code == 5 ]; then build=1 && insmod=0 && test=2;  fi;
+
+# If TestError, then success, success, fail
+if [ $code == 6 ]; then build=1 && insmod=1 && test=0; fi;
+
+echo "Set build=$build insmod=$insmod test=$insmod"; \
+if [ $build == 2 ]; then echo "##[set-output name=build;]N/A" && echo "##[set-output name=buildc;]grey"; fi;
+if [ $insmod == 2 ]; then echo "##[set-output name=insmod;]N/A" && echo "##[set-output name=insmodc;]grey"; fi;
+if [ $test == 2 ]; then echo "##[set-output name=test;]N/A" && echo "##[set-output name=testc;]grey"; fi;
+if [ $build == 1 ]; then echo "##[set-output name=build;]Passing" && echo "##[set-output name=buildc;]green"; fi;
+if [ $insmod == 1 ]; then echo "##[set-output name=insmod;]Passing" && echo "##[set-output name=insmodc;]green"; fi;
+if [ $test == 1 ]; then echo "##[set-output name=test;]Passing" && echo "##[set-output name=testc;]green"; fi;
+if [ $build == 0 ]; then echo "##[set-output name=build;]Failed" && echo "##[set-output name=buildc;]red"; fi;
+if [ $insmod == 0 ]; then echo "##[set-output name=insmod;]Failed" && echo "##[set-output name=insmodc;]red"; fi;
+if [ $test == 0 ]; then echo "##[set-output name=test;]Failed" && echo "##[set-output name=testc;]red"; fi;
+
 # Remove the test keys
 rm /tmp/test_key* include/key.h
 
 # Cleanup
 make clean
+
+# This script should technically always "succeed" so that the
+# badges are generated
+exit 0
