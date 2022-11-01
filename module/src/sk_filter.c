757bf2c59e9ecb39b6dda337fd9a510eef2ba8ee
landhb <landhb@users.noreply.github.com>
2022-10-31 23:01:19 -0400

    More notes on the 'why'.

--- module/src/sk_filter.c
+++ module/src/sk_filter.c
@@ -100,6 +100,9 @@ static bool db_sk_filter_charge(struct sock *sk, struct sk_filter *fp)
  * address is a usermode pointer.
  * 
  * @note Ported from the non-exported __sk_attach_prog in net/core/filter.c
+ * @note The actual underlying issue occurs when sk_attach_filter calls
+ * __get_filter, and attempts a copy_from_user() which fails when the sock_fprog
+ * contains a kernel pointer.
  */
 int sk_attach_prog(struct bpf_prog *prog, struct sock *sk) {
     struct sk_filter *fp = NULL, *old_fp = NULL;
