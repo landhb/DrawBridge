fb3af4655ddbca9e1fc1ba875460b42e63dfb0be
landhb <landhb@users.noreply.github.com>
2022-10-31 22:45:32 -0400

    Upgrade action, attempt new variables again.

--- module/tests/run.sh
+++ module/tests/run.sh
@@ -41,15 +41,15 @@ if [ $code == 5 ]; then build=1 && insmod=0 && test=2;  fi;
 if [ $code == 6 ]; then build=1 && insmod=1 && test=0; fi;
 
 echo "Set build=$build insmod=$insmod test=$insmod"; \
-if [ $build == 2 ]; then echo "{build}=N/A" >> $GITHUB_OUTPUT && echo "{buildc}=grey" >> $GITHUB_OUTPUT; fi;
-if [ $insmod == 2 ]; then echo "{insmod}=N/A" >> $GITHUB_OUTPUT  && echo "{insmodc}=grey" >> $GITHUB_OUTPUT; fi;
-if [ $test == 2 ]; then echo "{test}=N/A" >> $GITHUB_OUTPUT && echo "{testc}=grey" >> $GITHUB_OUTPUT; fi;
-if [ $build == 1 ]; then echo "{build}=Passing" >> $GITHUB_OUTPUT  && echo "{buildc}=green"  >> $GITHUB_OUTPUT; fi;
-if [ $insmod == 1 ]; then echo "{insmod}=Passing" >> $GITHUB_OUTPUT  && echo "{insmodc}=green"  >> $GITHUB_OUTPUT; fi;
-if [ $test == 1 ]; then echo "{test}=Passing" >> $GITHUB_OUTPUT  && echo "{testc}=green" >> $GITHUB_OUTPUT; fi;
-if [ $build == 0 ]; then echo "{build}=Failed" >> $GITHUB_OUTPUT  && echo "{buildc}=red" >> $GITHUB_OUTPUT; fi;
-if [ $insmod == 0 ]; then echo "{insmod}=Failed" >> $GITHUB_OUTPUT  && echo "{insmodc}=red" >> $GITHUB_OUTPUT; fi;
-if [ $test == 0 ]; then echo "{test}=Failed" >> $GITHUB_OUTPUT && echo "{testc}=red" >> $GITHUB_OUTPUT; fi;
+if [ $build == 2 ]; then echo "build=N/A" >> $GITHUB_OUTPUT && echo "buildc=grey" >> $GITHUB_OUTPUT; fi;
+if [ $insmod == 2 ]; then echo "insmod=N/A" >> $GITHUB_OUTPUT  && echo "insmodc=grey" >> $GITHUB_OUTPUT; fi;
+if [ $test == 2 ]; then echo "test=N/A" >> $GITHUB_OUTPUT && echo "testc=grey" >> $GITHUB_OUTPUT; fi;
+if [ $build == 1 ]; then echo "build=Passing" >> $GITHUB_OUTPUT  && echo "buildc=green"  >> $GITHUB_OUTPUT; fi;
+if [ $insmod == 1 ]; then echo "insmod=Passing" >> $GITHUB_OUTPUT  && echo "insmodc=green"  >> $GITHUB_OUTPUT; fi;
+if [ $test == 1 ]; then echo "test=Passing" >> $GITHUB_OUTPUT  && echo "testc=green" >> $GITHUB_OUTPUT; fi;
+if [ $build == 0 ]; then echo "build=Failed" >> $GITHUB_OUTPUT  && echo "buildc=red" >> $GITHUB_OUTPUT; fi;
+if [ $insmod == 0 ]; then echo "insmod=Failed" >> $GITHUB_OUTPUT  && echo "insmodc=red" >> $GITHUB_OUTPUT; fi;
+if [ $test == 0 ]; then echo "test=Failed" >> $GITHUB_OUTPUT && echo "testc=red" >> $GITHUB_OUTPUT; fi;
 
 # Remove the test keys
 rm /tmp/test_key* include/key.h
@@ -59,5 +59,5 @@ make clean
 
 # This script should technically always "succeed" so that the
 # badges are generated
-echo "##[set-output name=realexit;]$code"
+echo "realexit=$code" >> $GITHUB_OUTPUT
 exit 0
